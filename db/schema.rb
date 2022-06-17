7db74e2a37eb81eaf030ca22c59ae5471ef464c0
James <jimibue@gmail.com>
2022-06-17 10:53:45 -0600

    has many through

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_14_160746) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_17_162339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artist_songs", force: :cascade do |t|
+    t.bigint "artist_id", null: false
+    t.bigint "song_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_artist_songs_on_artist_id"
+    t.index ["song_id"], name: "index_artist_songs_on_song_id"
+  end
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -30,5 +45,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_14_160746) do
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "artist_songs", "artists"
+  add_foreign_key "artist_songs", "songs"
   add_foreign_key "books", "authors"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,9 @@
 
 Author.destroy_all
 Book.destroy_all
+ArtistSong.destroy_all
+Song.destroy_all
+Artist.destroy_all
 
 sk = Author.create(name:'Steven King', age:23)
 sc = Author.create(name:'Sean Carrol', age:53)
@@ -20,3 +23,46 @@ sc.books.create( title:'Blackholes', genre:'Science')
 
 puts "Authors in db #{Author.all.size}"
 puts "Books in db #{Book.all.size}"
+
+tool = Artist.create(name:'Tool')
+mj = Artist.create(name:'MJ')
+b = Artist.create(name:'Britney')
+
+s1 = Song.create(name:'46 & you, t-b')
+s2 = Song.create(name:'tool only, t')
+s3 = Song.create(name:'britney you are not ok, mj-b')
+s4 = Song.create(name:'mj and brit and tool song')
+s5 = Song.create(name:'brit and tool song 1')
+s6 = Song.create(name:'brit and tool song 2')
+s7 = Song.create(name:'mj and tool song 1')
+s8 = Song.create(name:'mj and tool song 2')
+
+ArtistSong.create(song_id: s1.id, artist_id: tool.id)
+ArtistSong.create(song_id: s1.id, artist_id: b.id)
+
+ArtistSong.create(song_id: s2.id, artist_id: tool.id)
+
+ArtistSong.create(song_id: s3.id, artist_id: b.id)
+ArtistSong.create(song_id: s3.id, artist_id: mj.id)
+
+ArtistSong.create(song_id: s4.id, artist_id: mj.id)
+ArtistSong.create(song_id: s4.id, artist_id: b.id)
+ArtistSong.create(song_id: s4.id, artist_id: tool.id)
+
+
+ArtistSong.create(song_id: s5.id, artist_id: b.id)
+ArtistSong.create(song_id: s5.id, artist_id: tool.id)
+
+ArtistSong.create(song_id: s6.id, artist_id: b.id)
+ArtistSong.create(song_id: s6.id, artist_id: tool.id)
+
+
+ArtistSong.create(song_id: s7.id, artist_id: mj.id)
+ArtistSong.create(song_id: s7.id, artist_id: tool.id)
+
+ArtistSong.create(song_id: s8.id, artist_id: mj.id)
+ArtistSong.create(song_id: s8.id, artist_id: tool.id)
+
+puts "artist size: #{Artist.all.size}"
+puts "songs size: #{Song.all.size}"
+puts "artist_songs size: #{ArtistSong.all.size}"
